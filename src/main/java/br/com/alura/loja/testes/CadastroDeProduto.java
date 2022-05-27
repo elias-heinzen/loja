46a9431cbdf7843e234cf51091f5a69917e3a138
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-05-26 21:57:19 -0300

    limitando dados de uma consulta

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -22,6 +22,9 @@ public class CadastroDeProduto {
 
         List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
         todos.forEach(p2 -> System.out.println(p2.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preco do Produto: " + precoDoProduto);
     }
 
     private static void cadastrarProduto() {
